6efa34d8cc58346372b0ce21228794f820ea70b5
sanggonlee <lee.sg16@gmail.com>
2019-01-26 17:21:53 -0500

    disallow querying user in the same request as login

--- types.go
+++ types.go
@@ -75,8 +75,7 @@ var MeType = graphql.NewObject(graphql.ObjectConfig{
 var UserSessionType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "userSession",
 	Fields: graphql.Fields{
-		"key":  &graphql.Field{Type: graphql.String},
-		"user": &graphql.Field{Type: UserType},
+		"key": &graphql.Field{Type: graphql.String},
 	},
 })
 
